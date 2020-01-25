9bc625e327aa9d968eb5fe68156db1a2eee6f7c4
Akash <subs@appsparkler.com>
2020-01-25 15:58:25 +0530

    basic firebase setup

---
+++ stories/firebase/setup.js
@@ -0,0 +1,28 @@
+mport firebase from 'firebase/app';
+import 'firebase/storage';
+
+const {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+} = process.env;
+
+
+const firebaseConfig = {
+  apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId,
+};
+
+
+const FirebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default FirebaseApp;
+
+export const putFile = (ref, file) => (FirebaseApp
+    .storage()
+    .ref(ref)
+    .put(file)
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const {resolve} = require('path')
+const {DefinePlugin} = require('webpack')
+const dotenv = require('dotenv')
 
 module.exports = {
+  plugins: [
+    new DefinePlugin({
+      ...dotenv.config().parse,
+    }),
+  ],
   resolve: {
     alias: {
       '@components': resolve('src/components'),
