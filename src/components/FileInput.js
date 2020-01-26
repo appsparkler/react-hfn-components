ec845b9edb99d9e49352bb581265c187d583ed85
Akash <subs@appsparkler.com>
2020-01-26 11:58:25 +0530

    setting the files to upload with setState

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
-export default function({config}) {
+export default function({config={}}) {
   return (
-    <input type="file" />
+    <input
+      type="file"
+      onInput={ setFilesToUpload.bind(null, config) }
+      {...config}
+    />
   )
+}
+
+function setFilesToUpload(config, evt) {
+  try {
+    const fileInput = evt.target
+    const filesReadyForUpload = []
+    for (let i = 0; i < fileInput.files.length; i++) {
+      filesReadyForUpload.push(evt.target.files[i])
+    }
+    config.setFilesToUpload([
+      ...config.filesToUpload,
+      ...evt.target.files,
+    ])
+  } catch (err) {
+    console.error(err)
+  }
 }
--- stories/firebase/FileInput.stories.js
+++ stories/firebase/FileInput.stories.js
@@ -7,10 +7,22 @@ export default {
 }
 
 export const defaultFileInput = () => {
-  const config = {FirebaseApp}
+  const [filesToUpload, setFilesToUpload] = React.useState([])
+  const config = {
+    FirebaseApp,
+    multiple: true,
+    filesToUpload,
+    setFilesToUpload,
+  }
   return (
     <form>
       <FileInput config={config} />
+      <input type="file" multiple />
+      {
+        filesToUpload.map((file, idx) => (
+          <div key="a">{file.name}, {file.size}</div>
+        ))
+      }
     </form>
   )
 }
