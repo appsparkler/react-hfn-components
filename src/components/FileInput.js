7cf5a15a070c52300ecdd22e59b353c089465a8e
Akash <subs@appsparkler.com>
2020-02-01 16:19:50 +0530

    refining the FileInput component

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {resolve} from 'path'
+// import {resolve} from 'path'
 
 const DEFAULT_CONFIG = {
   maxBytes: 5000,
@@ -9,32 +9,65 @@ export default React.forwardRef(function({config=DEFAULT_CONFIG}, ref) {
   return (
     <input
       type="file"
-      ref={ref}
-      className={ config.classNames }
-      onInput={ setFilesToUpload.bind(null, config) }
-      onChange={ () => console.log('changed...')}
-      multiple={config.multiple}
+      ref={ ref }
+      className={ config.classNames || '' }
+      onInput={ config.onInput || handleInput.bind(null, config, ref) }
+      onChange={ config.onChange || handleChange }
+      multiple={ config.multiple || false }
     />
   )
 })
 
-function setFilesToUpload({
-  // maxByteExceededCallback,
-  // maxBytes,
-  // setFilesToUpload,
-  path,
-  firebaseStorage,
-}, evt) {
+function handleChange() {
+  console.info('change not handled yet!')
+}
+
+// function uploadFiles({}) {
+//   // const {files} = this
+//   try {
+//     const puts = []
+//     for (let i = 0; i < files.length; i += 1) {
+//       console.log(path, path)
+//       const file = files[i]
+//       const filePath = resolve(path, file.name)
+//       const ref = firebaseStorage.ref(filePath)
+//       puts.push(ref.put(file))
+//     }
+//   } catch (e) {
+//
+//   }
+// }
+
+function setFilesToUpload(config, ref, evt) {
   try {
-    const files = evt.target.files
-    const puts = []
-    for (let i = 0; i < files.length; i += 1) {
-      console.log(path, path)
-      const file = files[i]
-      const filePath = resolve(path, file.name)
-      const ref = firebaseStorage.ref(filePath)
-      puts.push(ref.put(file))
+    if (config.filesToUpload && config.setFilesToUpload) {
+      config.setFilesToUpload([
+        ...evt.target.files,
+      ])
     }
+  } catch (e) {
+    throw new Error(e)
+  }
+}
+
+function handleInput(config, ref, evt) {
+  try {
+    setFilesToUpload(config, ref, evt)
+    // console.log('handling input')
+    // const state = {
+    //   files: evt.target.files,
+    //   maxBytes: config.maxBytes || 5000,
+    // }
+    // uploadFiles(config, evt)
+    // // const saveFiles
+    // const puts = []
+    // for (let i = 0; i < files.length; i += 1) {
+    //   console.log(path, path)
+    //   const file = files[i]
+    //   const filePath = resolve(path, file.name)
+    //   const ref = firebaseStorage.ref(filePath)
+    //   puts.push(ref.put(file))
+    // }
     // console.log(evt.target.files, storage)
     // const {target} = evt
     // const filesToUpload = [...target.files]
--- src/studio/App.js
+++ src/studio/App.js
@@ -4,10 +4,28 @@ import FirebaseApp from './firebase-app'
 
 export default () => {
   const firebaseStorage = FirebaseApp.storage()
+  const [filesToUpload, setFilesToUpload] = React.useState([])
+  const fileInputRef = React.useRef()
   const config = {
+    filesToUpload,
+    setFilesToUpload,
     firebaseStorage,
     multiple: true,
-    path: '/abcd/efghijk',
+    path: '/INABXK200',
   }
-  return (<FileInput config={config} />)
+  return (
+    <>
+      <FileInput config={config} ref={fileInputRef} />
+      <div>
+        {filesToUpload.map((file, idx) => {
+          return (
+            <pre key={`${file.name}-${idx}`}>
+              {file.name} - {file.size}bytes
+              <br />
+            </pre>
+          )
+        })}
+      </div>
+    </>
+  )
 }
