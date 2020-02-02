4512ed67b1197356a1a87ee5489ba7bc6fb3bfb4
Akash <subs@appsparkler.com>
2020-02-02 10:07:27 +0530

    - improved coverage - removed unnecessary lines of code

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -18,9 +18,7 @@ export default React.forwardRef(function({config=DEFAULT_CONFIG}, ref) {
   )
 })
 
-function handleChange() {
-  console.info('change not handled yet!')
-}
+function handleChange() {}
 
 // function uploadFiles({}) {
 //   // const {files} = this
@@ -39,49 +37,41 @@ function handleChange() {
 // }
 
 export function setFilesToUpload(config, ref, evt) {
-  try {
-    if (config.filesToUpload && config.setFilesToUpload) {
-      config.setFilesToUpload([
-        ...evt.target.files,
-      ])
-    }
-  } catch (e) {
-    throw new Error(e)
+  if (config.filesToUpload && config.setFilesToUpload) {
+    config.setFilesToUpload([
+      ...evt.target.files,
+    ])
   }
 }
 
 export function handleInput(config, ref, evt) {
-  try {
-    setFilesToUpload(config, ref, evt)
-    // console.log('handling input')
-    // const state = {
-    //   files: evt.target.files,
-    //   maxBytes: config.maxBytes || 5000,
-    // }
-    // uploadFiles(config, evt)
-    // // const saveFiles
-    // const puts = []
-    // for (let i = 0; i < files.length; i += 1) {
-    //   console.log(path, path)
-    //   const file = files[i]
-    //   const filePath = resolve(path, file.name)
-    //   const ref = firebaseStorage.ref(filePath)
-    //   puts.push(ref.put(file))
-    // }
-    // console.log(evt.target.files, storage)
-    // const {target} = evt
-    // const filesToUpload = [...target.files]
-    // const totalBytes = filesToUpload.reduce((r, file) => r + file.size, 0)
-    // if (totalBytes > maxBytes) {
-    //   maxByteExceededCallback.call(null, arguments)
-    //   target.value = null
-    //   setFilesToUpload([])
-    // } else {
-    //   setFilesToUpload([
-    //     ...target.files,
-    //   ])
-    // }
-  } catch (err) {
-    console.error(err)
-  }
+  setFilesToUpload(config, ref, evt)
+  // console.log('handling input')
+  // const state = {
+  //   files: evt.target.files,
+  //   maxBytes: config.maxBytes || 5000,
+  // }
+  // uploadFiles(config, evt)
+  // // const saveFiles
+  // const puts = []
+  // for (let i = 0; i < files.length; i += 1) {
+  //   console.log(path, path)
+  //   const file = files[i]
+  //   const filePath = resolve(path, file.name)
+  //   const ref = firebaseStorage.ref(filePath)
+  //   puts.push(ref.put(file))
+  // }
+  // console.log(evt.target.files, storage)
+  // const {target} = evt
+  // const filesToUpload = [...target.files]
+  // const totalBytes = filesToUpload.reduce((r, file) => r + file.size, 0)
+  // if (totalBytes > maxBytes) {
+  //   maxByteExceededCallback.call(null, arguments)
+  //   target.value = null
+  //   setFilesToUpload([])
+  // } else {
+  //   setFilesToUpload([
+  //     ...target.files,
+  //   ])
+  // }
 }
