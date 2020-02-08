47bc8d79b601b5a04d62f0d986978d0d3b9de1c4
Aakash Shah <ashah@lowes.com>
2020-02-08 21:00:32 +0530

    Moved Hooks outside of components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 export FileInput from './components/FileInput'//eslint-disable-line
 export setFirebaseApp from './singletons/firebase-storage-api' // eslint-disable-line
-export useFileInputConfig from './components/hooks/FileInputConfig'// eslint-disable-line
+export useFileInputConfig from './hooks/FileInputConfig'// eslint-disable-line
