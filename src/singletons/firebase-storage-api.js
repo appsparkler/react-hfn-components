8ba7bb906ffce9b7500977b81d9ad58734479b9f
Aakash Shah <ashah@lowes.com>
2020-02-08 21:25:06 +0530

    clean up

--- src/singletons/firebase-storage-api.js
+++ src/singletons/firebase-storage-api.js
@@ -1,12 +1,3 @@
-// export default {
-//   get app() {
-//     return this._firebaseApp
-//   },
-//   set app(firebaseApp) {
-//     this._firebaseApp = firebaseApp
-//   },
-// }
-
 export let FirebaseApp = null
 
 export default (app) => {
