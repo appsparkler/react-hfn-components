39f11b90e910325a5bfe1725b6ddc35e48aa7649
Aakash Shah <ashah@lowes.com>
2020-02-15 11:37:50 +0530

    WIP

--- studio/App.js
+++ studio/App.js
@@ -4,11 +4,15 @@ import {FileInputV2} from '@appsparkler/react-hfn-components'
 export default () => {
   const keydx = 'xyz'
   const params = {
+    // input-attributes
     type: 'file',
     multiple: true,
-    path: '/abhyasiDaya/INAAAB998',
     className: 'my-special-input-field',
 
+    // Other upload information
+    path: '/abhyasiDaya/INAAAB998',
+
+    // Validations
     maxBytes: 50000,
     maxFiles: 5,
     allowedFileTypes: ['png', 'jpg', 'pdf'],
