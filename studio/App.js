e62121267c45c4689d785dcd93666a97518e0f6b
Aakash Shah <ashah@lowes.com>
2020-02-15 16:15:35 +0530

    Setting label

--- studio/App.js
+++ studio/App.js
@@ -2,14 +2,13 @@ import React from 'react'
 import {FileInput} from '@appsparkler/react-hfn-components'
 import FirebaseApp from './firebase-app'
 
-// setFirebaseApp(FirebaseApp)
 import './styles.sass'
 
 export default () => {
   const storageRef = FirebaseApp.storage().ref('id-proofs')
   const keydx = 'xyz'
   const params = {
-    // label: 'Id Proofs',
+    label: 'Id Proofs',
     // input-attributes
     className: 'my-special-input-field',
     onUpload: (...args) => {
