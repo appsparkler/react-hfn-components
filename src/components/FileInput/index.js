66817d5e2e0626cb957c7ac125017d7f66487eaf
Aakash Shah <ashah@lowes.com>
2020-02-17 01:42:58 +0530

    WIP

--- src/components/FileInput/index.js
+++ src/components/FileInput/index.js
@@ -1,16 +1,5 @@
 import React from 'react'
 
-function useFileInput({props}) {
-  const {setFile} = props
-  const handleInput = (evt) => {
-    const file = evt.target.files.item(0)
-    setFile(file)
-  }
-  return {
-    handleInput,
-  }
-}
-
 function FileInput(props) {
   const {handleInput} = useFileInput({props})
   return (
