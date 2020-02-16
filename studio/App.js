f30235a555bfe42440b4052e02d740ca0f6c7351
Aakash Shah <ashah@lowes.com>
2020-02-16 14:36:24 +0530

    getting files output

--- studio/App.js
+++ studio/App.js
@@ -11,29 +11,9 @@ export default () => {
     label: 'Id Proofs',
     // input-attributes
     className: 'my-special-input-field',
-    onUpload: ({props, states}) => {
-      const {uploadDetails} = states
-      if (uploadDetails && uploadDetails.length) {
-        const uploadTasks = uploadDetails.map((item) => item.uploadTask)
-        console.log('upload tasks: ', uploadTasks)
-        uploadTasks.forEach((task) => {
-          task.on(
-              'state_changed',
-              (snapshot) => console.log(snapshot),
-              (err) => conosle.log('upload-error', err),
-              () => console.log('upload-complete'),
-          )
-        })
-        const snapshots = uploadTasks.map((item)=> item.snapshot)
-        snapshots.forEach((snapshot) => {
-          console.log('snapshot : ', snapshot)
-          const ref = snapshot.ref
-          console.log('ref : ', ref)
-          const downloadURL = ref.getDownloadURL()
-          downloadURL.then((url) => {
-            console.log('download-url: ', url)
-          })
-        })
+    onUpload: (files) => {
+      if (files && files.length) {
+        debugger
       }
     },
     required: true,
