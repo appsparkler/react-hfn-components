da4120fe8c51f207878f336d196234214e8687af
Aakash Shah <ashah@lowes.com>
2020-02-19 06:51:52 +0530

    Setting state when is-verifying

--- src/components/FileLink/index.js
+++ src/components/FileLink/index.js
@@ -2,12 +2,19 @@ import React from 'react'
 import useFileLink from './useFileLink'
 
 function FileLink(props) {
-  const {file} = useFileLink({props})
+  const {file, isVerifying} = useFileLink({props})
   return (
     <div className="FileLink">
-      <a href={file && file.downloadUrl} target="__blank">
-        {file && file.fileName}
-      </a>
+      {
+        isVerifying && 'Is Verifying...'
+      }
+      {
+        !isVerifying && file && (
+          <a href={file && file.downloadUrl} target="__blank">
+            {file && file.fileName}
+          </a>
+        )
+      }
     </div>
   )
 }
