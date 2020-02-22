e1ae4d4bb20ba93a943c3ca7640ab4a499e354ee
Aakash Shah <ashah@lowes.com>
2020-02-22 16:24:52 +0530

    Moved Photo preview to separate component

---
+++ src/components/FirebasePhotoInput/CroppieInput/PreviewImage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PreviewImage = (props) => {
+  return (
+    <div className="PreviewImage">
+      <h3>Preview</h3>
+      <img src="" ref={props?.photoPreviewRef} />
+    </div>
+  )
+}
+
+export default PreviewImage
--- src/components/FirebasePhotoInput/CroppieInput/index.js
+++ src/components/FirebasePhotoInput/CroppieInput/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PreviewImage from './PreviewImage'
 import useCroppieInput from './useCroppieInput'
 import 'croppie/croppie.css'
 
