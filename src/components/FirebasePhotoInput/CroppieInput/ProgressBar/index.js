04ed62cd69300dd1f0fe57a5fdb41490549b22ed
Aakash Shah <ashah@lowes.com>
2020-02-23 11:27:05 +0530

    Setting progress %age

--- src/components/FirebasePhotoInput/CroppieInput/ProgressBar/index.js
+++ src/components/FirebasePhotoInput/CroppieInput/ProgressBar/index.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types'
 const ProgressBar = ({progress}) => (
   <div className="ProgressBar">
     <progress min="0" max="100" value={progress} />
+    <br />
+    <pre>{progress?.toFixed(2)}%</pre>
   </div>
 )
 
