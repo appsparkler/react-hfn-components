08fbce4b4a49ec6bb993a5b99dff10fa8f5a6da6
Aakash Shah <ashah@lowes.com>
2020-02-23 11:39:01 +0530

    Added classname for upload button

--- src/components/FirebasePhotoInput/CroppieInput/UploadButton/index.js
+++ src/components/FirebasePhotoInput/CroppieInput/UploadButton/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const UploadButton = (props) => {
   const {uploadPhoto} = useUploadButtonProps({props})
   return (
-    <span>
+    <span className="UploadButton">
       <button onClick={uploadPhoto}>Upload Photo</button>
     </span>
   )
