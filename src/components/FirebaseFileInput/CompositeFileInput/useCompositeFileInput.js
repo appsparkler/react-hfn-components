91d1f44e94d69862f4160e38f6e1471d48aa5838
Aakash Shah <ashah@lowes.com>
2020-02-29 08:12:38 +0530

    Clean up and restructuring

--- src/components/FirebaseFileInput/CompositeFileInput/useCompositeFileInput.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default ({props}) => {
-  const [isUploading, setIsUploading] = React.useState(false)
-  return {
-    ...props,
-    isUploading, setIsUploading,
-  }
-}
--- src/components/FirebaseFileInput/index.js
+++ src/components/FirebaseFileInput/index.js
@@ -1,64 +1,17 @@
 import React from 'react'
-import ReadOnlyFileInput from './ReadOnlyFileInput'
-import CompositeFileInput from './CompositeFileInput'
-import './styles.sass'
-import useFirebaseFileInput from './useFirebaseFileInput'
+import PropTypes from 'prop-types'
 
-export default (props) => {
-  const compositeProps = useFirebaseFileInput({props})
-  const {readOnly} = compositeProps
-  // return (
-  //   <div>
-  //     <div className="File-UnAvailable-State">
-  //       <p>Upload Passport *</p>
-  //       <button className="button">
-  //         Upload File
-  //       </button>
-  //     </div>
-  //
-  //     <div className="File-UnAvailable-State">
-  //       <p>Upload Passport *</p>
-  //       <button className="button" disabled>
-  //         Uploading...
-  //       </button>
-  //     </div>
-  //
-  //     <div className="File-Available-State verifying">
-  //       <p>Upload Passport *</p>
-  //       <span>Verifying...</span>
-  //     </div>
-  //
-  //     <div className="File-Available-State">
-  //       <p>Upload Passport *</p>
-  //       <a href="">My passport</a>
-  //       <button className="button">
-  //         Edit
-  //       </button>
-  //     </div>
-  //
-  //     <div className="File-Available-State">
-  //       <p>Upload Passport *</p>
-  //       <span href="">My passport</span>
-  //       <button className="button" disabled>
-  //         Editing
-  //       </button>
-  //     </div>
-  //
-  //     <h1>Read Only Version</h1>
-  //     <div className="ReadOnly-File-Available-State">
-  //       <a href="">My passport</a>
-  //     </div>
-  //
-  //     <div className="ReadOnly-File-UnAvailable-State">
-  //       <p>No file is uploaded.</p>
-  //     </div>
-  //
-  //   </div>
-  // )
+const FirebaseFileInput = (props) => {
   return (
-    <div className="React-HFN-FirebaseFileInput">
-      {readOnly && <ReadOnlyFileInput {...compositeProps} /> }
-      {!readOnly && <CompositeFileInput {...compositeProps} />}
+    <div>
+      {props.readOnly && <h2>ReadOnly Version</h2>}
+      {!props.readOnly && <h2>Editable Version</h2>}
     </div>
   )
 }
+
+FirebaseFileInput.propTypes = {
+  readOnly: PropTypes.bool.isRequired,
+}
+
+export default FirebaseFileInput
