adf1d27093fe174bf0ff29e22fd470d719ede266
Aakash Shah <ashah@lowes.com>
2020-03-01 15:01:59 +0530

    Renaming

--- studio/App.js
+++ studio/App.js
@@ -1,15 +1,12 @@
 import React from 'react'
-// import config from './config'
-// import {FirebasePhotoInput} from '@appsparkler/react-hfn-components'
-import Test from '@react-hfn-components/Test'
+import config from './config'
+import {FirebasePhotoInput} from '@appsparkler/react-hfn-components'
 import './styles.sass'
 
 const StudioApp = () => {
   return (
     <div>
-      <Test initialCount={12} />
-      <Test initialCount={20} />
-      <Test initialCount={-20} />
+      <FirebasePhotoInput {...config} />
     </div>
   )
 }
