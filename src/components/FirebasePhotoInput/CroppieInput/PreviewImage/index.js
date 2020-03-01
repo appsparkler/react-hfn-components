fbea10a9a5a1d3117819de24c04d07f8ead5f32f
Aakash Shah <ashah@lowes.com>
2020-03-01 11:53:05 +0530

    Updated PropTypes

--- src/components/FirebasePhotoInput/CroppieInput/PreviewImage/index.js
+++ src/components/FirebasePhotoInput/CroppieInput/PreviewImage/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const PreviewImage = (props) => {
   return (
     <div className="PreviewImage col-xs-4">
       <h3 className="text-center text-muted">Preview</h3>
-      <img className="center-block" src="" ref={props?.photoPreviewRef} />
+      <img className="center-block"
+        src=""
+        ref={props && props.photoPreviewRef}
+      />
     </div>
   )
+}
+
+PreviewImage.propTypes = {
+  photoPreviewRef: PropTypes.object,
 }
 
 export default PreviewImage
