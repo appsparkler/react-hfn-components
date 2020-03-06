5fdfa8c2b4b1a3ff59312828accccb854fe08ac4
Aakash Shah <ashah@lowes.com>
2020-03-06 06:09:28 +0530

    exporting file-link connector

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@
 export FirebaseFileInput from '@react-hfn-components/FirebaseFileInput' //eslint-disable-line
 export {connectFirebasePhotoInput} from '@react-hfn-components/FirebasePhotoInput/context' // eslint-disable-line
 // export FirebasePhotoContext
+export {
+  connectFirebaseFileLink,
+} from '@react-hfn-components/FirebaseFileLink/context'
