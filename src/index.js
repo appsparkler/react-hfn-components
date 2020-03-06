7c37850d99ce292fb71ef0ea1f26579efd10e93b
Aakash Shah <ashah@lowes.com>
2020-03-06 06:01:56 +0530

    setup lint-staged

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
 /* Components*/
 export FirebaseFileInput from '@react-hfn-components/FirebaseFileInput' //eslint-disable-line
-export {connectFirebasePhotoInput} from '@react-hfn-components/FirebasePhotoInput/context' // eslint-disable-line
+  export {connectFirebasePhotoInput} from '@react-hfn-components/FirebasePhotoInput/context' // eslint-disable-line
 // export FirebasePhotoContext
