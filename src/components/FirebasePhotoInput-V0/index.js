60f18dda3cd32ed316613d735c6868541b1776c4
Aakash Shah <ashah@lowes.com>
2020-03-06 05:59:52 +0530

    Deleted comments & resolved eslint errors

--- src/components/FirebasePhotoInput-V0/index.js
+++ src/components/FirebasePhotoInput-V0/index.js
@@ -8,7 +8,7 @@ import './styles.sass'
 
 const FirebasePhotoInput = (props) => {
   const superProps = useFirebasePhotoInput({props})
-  const {selectedFile, exceedsMaxBytes} = superProps
+  const {selectedFile} = superProps
   return (
     <div className="ReactHFN_FirebasePhotoInput">
       <div className="CurrentPhotoAndFileInput col-xs-12 col-sm-5 col-lg-3">
@@ -20,26 +20,6 @@ const FirebasePhotoInput = (props) => {
           {selectedFile && <CroppieInput {...superProps}/>}
         </div>
       </div>
-      {/*
-            <div className="state">
-              <h2>No File Uploaded State</h2>
-              <FileInput {...superProps} />
-            </div>
-
-            <div className="state">
-              <h2>Exceeds MaxBytes</h2>
-              {superProps?.exceedsMaxBytes && <p>Exceeds max Bytes...</p>}
-            </div>
-
-            <div className="state">
-              <CurrentPhoto {...superProps} />
-            </div>
-
-            <div className="state">
-              <h2>Croppie</h2>
-              {selectedFile && <CroppieInput {...superProps}/>}
-            </div>
-      */}
     </div>
   )
 }
