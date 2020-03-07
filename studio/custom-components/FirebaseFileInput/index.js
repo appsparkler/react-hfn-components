2fb0773affd881f87454b3a60e1f24e92571cd8e
Aakash Shah <ashah@lowes.com>
2020-03-07 16:28:45 +0530

    restructured firebase-file-input example

--- studio/custom-components/FirebaseFileInput/index.js
+++ studio/custom-components/FirebaseFileInput/index.js
@@ -5,24 +5,6 @@ import {
 import PropTypes from 'prop-types'
 import config from './config'
 
-// const CustomFirebaseFileLink =
-// connectFirebaseFileLink(({isVerifying, file}) => {
-//   return (<div className="container">
-//     {isVerifying && 'Verifying...'}
-//     {
-//       file && (
-//         <a
-//           href={file.downloadURL}
-//           target="__blank"
-//         >
-//           {file.fileName}
-//         </a>
-//       )
-//     }
-//     {!file && !isVerifying && 'No File Found...'}
-//   </div>)
-// }, config)
-
 const CustomFirebaseFileLink = ({isVerifying, file}) => {
   return (<div className="container">
     {isVerifying && 'Verifying...'}
@@ -52,74 +34,75 @@ const CustomFirebaseFileInput = ({
   maxBytes, maxBytesExceeded,
   isUploading, progress, uploaded,
 }) => {
-  return (<div className="container">
-    <h2>Custtom File Input</h2>
-    <table className="table table-striped table-bordered">
-      <tbody>
-        <tr>
-          <td>
-            <h3>File Link</h3>
-            {!isUploading && <CustomFirebaseFileLink
-              file={file}
-              isVerifying={isVerifying}
-            />}
-            {isUploading && 'Uploading new file...'}
-          </td>
-          <td>
-            <h3>Input Field</h3>
-            {/* PASS label field as you like
+  return (
+    <div className="container">
+      <h2>Custtom File Input</h2>
+      <table className="table table-striped table-bordered">
+        <tbody>
+          <tr>
+            <td>
+              <h3>File Link</h3>
+              {!isUploading && <CustomFirebaseFileLink
+                file={file}
+                isVerifying={isVerifying}
+              />}
+              {isUploading && 'Uploading new file...'}
+            </td>
+            <td>
+              <h3>Input Field</h3>
+              {/* PASS label field as you like
             (it will not come from a variable)
             */}
-            <label>
-              {!file && 'Upload Visa'}
-              {file && 'Edit Visa'}
-            </label>
-            {/* PASS attributes such as required, disabled
+              <label>
+                {!file && 'Upload Visa'}
+                {file && 'Edit Visa'}
+              </label>
+              {/* PASS attributes such as required, disabled
             (they will not come as variables)
 
             handleInput and type need to be passed
             since we are processing them
             */}
-            <input
-              type={type}
-              required={true}
-              onInput={handleInput}
-            />
-          </td>
-          <td>
-            <h3>Max Bytes Message:</h3>
-            <p className="help-block">
+              <input
+                type={type}
+                required={true}
+                onInput={handleInput}
+              />
+            </td>
+            <td>
+              <h3>Max Bytes Message:</h3>
+              <p className="help-block">
               Max Bytes: {maxBytes && maxBytes.toFixed(2)} kB
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <h3>Max Bytes Exceeded Errors:</h3>
-            {maxBytesExceeded && (
-              <p className="alert alert-danger">Max Bytes exceeded!</p>
-            )}
-          </td>
-          <td colSpan="2">
-            <h3>Progress Bar: </h3>
-            {isUploading && (
-              <div>
-                <progress
-                  min="0"
-                  max="100"
-                  value={progress}
-                />
-                <br />
-                <p>{progress.toFixed(2)}%</p>
-              </div>
-            )}
-            {isUploading && <span>Uploading...</span>}
-            {uploaded && <pre>Uploaded...</pre>}
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>)
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h3>Max Bytes Exceeded Errors:</h3>
+              {maxBytesExceeded && (
+                <p className="alert alert-danger">Max Bytes exceeded!</p>
+              )}
+            </td>
+            <td colSpan="2">
+              <h3>Progress Bar: </h3>
+              {isUploading && (
+                <div>
+                  <progress
+                    min="0"
+                    max="100"
+                    value={progress}
+                  />
+                  <br />
+                  <p>{progress.toFixed(2)}%</p>
+                </div>
+              )}
+              {isUploading && <span>Uploading...</span>}
+              {uploaded && <pre>Uploaded...</pre>}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>)
 }
 
 CustomFirebaseFileInput.propTypes = {
