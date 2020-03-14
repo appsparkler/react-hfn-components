c7c3aa53e771f91c031a4f69d2b007217a06b459
Aakash Shah <ashah@lowes.com>
2020-03-14 10:47:56 +0530

    Added BS-4 file-input for FileInput component

--- studio/custom-components/FirebaseFileInput/index.js
+++ studio/custom-components/FirebaseFileInput/index.js
@@ -36,7 +36,7 @@ const CustomFirebaseFileInput = ({
 }) => {
   return (
     <div className="container">
-      <h2>Custtom File Input</h2>
+      <h2>Custom File Input</h2>
       <table className="table table-striped table-bordered">
         <tbody>
           <tr>
@@ -58,16 +58,26 @@ const CustomFirebaseFileInput = ({
                 {file && 'Edit Visa'}
               </label>
               {/* PASS attributes such as required, disabled
-            (they will not come as variables)
+                (they will not come as variables)
 
-            handleInput and type need to be passed
-            since we are processing them
-            */}
-              <input
-                type={type}
-                required={true}
-                onInput={handleInput}
-              />
+                handleInput and type need to be passed
+                since we are processing them
+              */}
+              <div className="custom-file">
+                <input
+                  type={type}
+                  required={true}
+                  onInput={handleInput}
+                  className="custom-file-input"
+                  id="customFile"
+                />
+                <label
+                  className="custom-file-label"
+                  htmlFor="customFile"
+                >
+                  Choose file
+                </label>
+              </div>
             </td>
             <td>
               <h3>Max Bytes Message:</h3>
@@ -117,85 +127,6 @@ CustomFirebaseFileInput.propTypes = {
   uploaded: PropTypes.bool,
   progress: PropTypes.number,
 }
-// const CustomFirebaseFileInput = (props, ref) => {
-//   const {
-//     // YOU HAVE ALL THESE PROPS FOR YOUR CUSTOM COMPONENT
-//     type, label, disabled, required, name,
-//     progress, isUploading, uploaded,
-//     maxBytes, maxBytesError,
-//     isVerifying, file,
-//     handleInput,
-//     maxBytesExceeded,
-//   } = props
-//   return (
-//     <div>
-//       <h2>Custom File Input</h2>
-//       <table>
-//         <tr>
-//           <td>
-//             <h3>File Details:</h3>
-//             {isVerifying && 'isVerifying...'}
-//             {!file && !isVerifying && 'No File Uploaded...'}
-//             {file && !isVerifying && (
-//               <a href={file.downloadURL}>{file.fileName}</a>
-//             )}
-//             <br />
-//           </td>
-//           <td>
-//             <h3>Action: </h3>
-//             <h4>{!file && 'Upload File'}</h4>
-//             <h4>{file && 'Edit File'}</h4>
-//           </td>
-//           <td>
-//             <h3>Label</h3>
-//             <label>
-//               <span>{label}</span>
-//               <span>{required && '*'}</span>
-//             </label>
-//           </td>
-//           <td>
-//             <h3>Input Field</h3>
-//             {/* PASS THE REF TO THE INPUT ALONG WITH THESE PROPS */}
-//             <input
-//               type={type}
-//               name={name}
-//               onInput={handleInput}
-//               ref={ref}
-//               disabled={disabled}
-//               required={required}
-//             />
-//           </td>
-//         </tr>
-//         <tr>
-//           <td>
-//             <h3>Max Bytes Message:</h3>
-//             <p className="help-block">
-//               Max Bytes: {maxBytes && maxBytes.toFixed(2)} kB
-//             </p>
-//           </td>
-//           <td>
-//             <h3>Max Bytes Exceeded Errors:</h3>
-//             {maxBytesExceeded && (
-//               <p className="alert alert-danger">{maxBytesError}</p>
-//             )}
-//           </td>
-//           <td colSpan="2">
-//             <h3>Progress Bar: </h3>
-//             {(progress > 0) && !uploaded && (
-//               <React.Fragment>
-//                 <progress min="0" max="100" value={progress} />
-//                 <br />
-//                 {progress.toFixed(2)}%
-//               </React.Fragment>
-//             )}
-//             {isUploading && <pre>Is Uploading...</pre>}
-//             {uploaded && <pre>Uploaded...</pre>}
-//           </td>
-//         </tr>
-//       </table>
-//     </div>
-//   )
-// }
 
 CustomFirebaseFileInput.propTypes = {
   // type: PropTypes.string,
