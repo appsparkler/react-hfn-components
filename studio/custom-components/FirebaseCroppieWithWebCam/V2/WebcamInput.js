1141d38ae5aff3080ac411d9798affdc55d83d3d
Aakash Shah <ashah@lowes.com>
2020-03-15 07:39:13 +0530

    Setting up webcam

--- studio/custom-components/FirebaseCroppieWithWebCam/V2/WebcamInput.js
+++ studio/custom-components/FirebaseCroppieWithWebCam/V2/WebcamInput.js
@@ -1,7 +1,44 @@
 import React from 'react'
+import useWebCam from './useWebCam'
 
-const WebCamInput = ({}) => (
-  <div>Web Cam Input</div>
-)
+const WebCamInput = ({}) => {
+  const {
+    videoRef,
+    canvasRef,
+    photoRef,
+    buttonRef,
+  } = useWebCam()
+  return (
+    <div className="camera">
+      <video id="video"
+        ref={videoRef}
+        style={{outline: '12px slateBlue solid'}}
+      >
+        Video stream not available.
+      </video>
+      <button id="startbutton" ref={buttonRef}>
+        Take photo
+      </button>
+
+      <div className="position-fixed" style={{left: '10000px'}}>
+        {
+          <canvas id="canvas"
+            ref={canvasRef}
+            style={{outline: '20px midnightBlue solid'}}
+          >
+          </canvas>
+        }
+
+        <div className="output">
+          <h3>IMAGE...</h3>
+          <img
+            id="photo"
+            alt="The screen capture will appear in this box."
+            ref={photoRef}/>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default WebCamInput
