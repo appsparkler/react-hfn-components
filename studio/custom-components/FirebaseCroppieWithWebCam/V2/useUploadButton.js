2eebef40c523a993c2e456eadaa971e06bc067b9
Aakash Shah <ashah@lowes.com>
2020-03-15 14:35:34 +0530

    Setting upload button

--- studio/custom-components/FirebaseCroppieWithWebCam/V2/useUploadButton.js
+++ studio/custom-components/FirebaseCroppieWithWebCam/V2/useUploadButton.js
@@ -1,9 +1,12 @@
 // import React from 'react'
+import {dataURL2Blob} from './utils'
 
 export default ({croppedDataURL}) => {
   return {
     handleClick: (evt) => {
-      croppedDataURL
+      const blob = dataURL2Blob(croppedDataURL)
+      const file = new File([blob], 'pic')
+      debugger
     },
   }
 }
