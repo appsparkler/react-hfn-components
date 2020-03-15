68ce6a9ba6b1fe5ec2b7a42e1e17470319fcb7aa
Aakash Shah <ashah@lowes.com>
2020-03-15 15:29:39 +0530

    formatting

--- studio/custom-components/FirebaseCroppieWithWebCam/V2/UploadButton.js
+++ studio/custom-components/FirebaseCroppieWithWebCam/V2/UploadButton.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import useUploadButton from './useUploadButton'
 
-
 const UploadButtton = ({
   croppedDataURL, storageRef, maxBytes, onStateChange, onError, onDone,
 }) => {
