1547c27422372f19296de7f8969191dd05ecd543
Aakash Shah <ashah@lowes.com>
2020-03-15 12:27:34 +0530

    setting up croppie

--- studio/custom-components/FirebaseCroppieWithWebCam/V2/Croppie.js
+++ studio/custom-components/FirebaseCroppieWithWebCam/V2/Croppie.js
@@ -2,8 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import useCroppie from './useCroppie'
 
-const Croppie = ({dataURL}) => {
-  const {croppieRef} = useCroppie({dataURL})
+const Croppie = ({dataURL, croppieConfig}) => {
+  const {croppieRef} = useCroppie({dataURL, croppieConfig})
   return (
     <div>
       <div ref={croppieRef}></div>
@@ -13,6 +13,14 @@ const Croppie = ({dataURL}) => {
 
 Croppie.propTypes = {
   dataURL: PropTypes.string.isRequired,
+  croppieConfig: PropTypes.object,
+}
+
+Croppie.defaultProps = {
+  croppieConfig: {
+    viewport: {width: 100, height: 100},
+    boundary: {width: 300, height: 300},
+  },
 }
 
 export default Croppie
--- studio/custom-components/FirebaseCroppieWithWebCam/V2/useCroppie.js
+++ studio/custom-components/FirebaseCroppieWithWebCam/V2/useCroppie.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import Croppie from 'croppie'
 
-function componentDidMount({croppieRef, dataURL}) {
-  const croppie = new Croppie(croppieRef.current, {
-    viewport: {width: 100, height: 100},
-    boundary: {width: 300, height: 300},
-  })
-  croppie.bind({
-    url: dataURL,
-  })
+function dataURLDidChange({
+  croppie, dataURL, croppieRef, setCroppie, croppieConfig,
+}) {
+  if (croppie) {
+    croppie.bind({
+      url: dataURL,
+    })
+  } else {
+    const croppie = new Croppie(croppieRef.current, croppieConfig)
+    croppie.bind({
+      url: dataURL,
+    })
+    setCroppie(croppie)
+  }
 }
 
-export default ({dataURL}) => {
+export default ({dataURL, croppieConfig}) => {
   const croppieRef = React.useRef()
-  React.useEffect(componentDidMount.bind(null, {
-    croppieRef, dataURL,
-  }), [])
+  const [croppie, setCroppie] = React.useState()
+  React.useEffect(dataURLDidChange.bind(null, {
+    croppie, dataURL, croppieRef, setCroppie, croppieConfig,
+  }), [dataURL])
   return {
     croppieRef,
   }
