0400a438607015931de6ae04250df04a8c48071f
Aakash Shah <ashah@lowes.com>
2020-03-15 11:16:39 +0530

    Setup fileInput hook

--- studio/custom-components/FirebaseCroppieWithWebCam/V2/FileInput.js
+++ studio/custom-components/FirebaseCroppieWithWebCam/V2/FileInput.js
@@ -1,25 +1,39 @@
 import React from 'react'
+import useFileInput from './useFileInput'
 
-const FileInput = ({}) => (
-  <div className="input-group mb-3">
-    <div className="input-group-prepend">
-      <span
-        className="input-group-text">
-        Upload Photo
-      </span>
+const FileInput = ({}) => {
+  const {handleChange, dataURL} = useFileInput()
+  return (
+    <div>
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span
+            className="input-group-text">
+            Upload Photo
+          </span>
+        </div>
+        <div className="custom-file">
+          <input
+            type="file"
+            className="custom-file-input"
+            onChange={handleChange}
+          />
+          <label
+            className="custom-file-label"
+          >
+            Choose file
+          </label>
+        </div>
+      </div>
+      {dataURL && (
+        <pre className="pre-scrollable">
+          <code style={{whiteSpace: 'normal'}}>
+            {dataURL}
+          </code>
+        </pre>
+      )}
     </div>
-    <div className="custom-file">
-      <input
-        type="file"
-        className="custom-file-input"
-      />
-      <label
-        className="custom-file-label"
-      >
-        Choose file
-      </label>
-    </div>
-  </div>
-)
+  )
+}
 
 export default FileInput
---
+++ studio/custom-components/FirebaseCroppieWithWebCam/V2/useFileInput.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function handleFileLoaded({setDataURL}, evt) {
+  const dataURL = evt.target.result
+  setDataURL(dataURL)
+}
+
+function handleChange({
+  setDataURL,
+}, evt) {
+  evt.preventDefault()
+  evt.stopPropagation()
+  const {files} = evt.target
+  const file = files.item(0)
+  if (file) {
+    const reader = new FileReader()
+    reader.addEventListener(
+        'load',
+        handleFileLoaded.bind(null, {setDataURL}),
+    )
+    reader.readAsDataURL(file)
+  }
+}
+
+export default () => {
+  const [dataURL, setDataURL] = React.useState()
+  return {
+    dataURL,
+    handleChange: handleChange.bind(null, {setDataURL}),
+  }
+}
