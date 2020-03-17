64228a97864fa6728cfa2aa2ea36870e663eea4c
Aakash Shah <ashah@lowes.com>
2020-03-17 22:00:38 +0530

    setting up useFileInput hook

--- studio/custom-components/FirebaseCroppieWithWebCam/V3/index.js
+++ studio/custom-components/FirebaseCroppieWithWebCam/V3/index.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types'
 import config from './config'
 import connectFirebaseSuperCroppie from './connectFirebaseSuperCroppie'
 
-const FirebaseSuperCroppie = ({file, isVerifying}) => {
+const FirebaseSuperCroppie = ({
+  file, isVerifying, handleChange,
+}) => {
+  const mediaSource = 'file'
   return (
     <div className="container mt-2">
       <div className="row">
@@ -13,6 +16,31 @@ const FirebaseSuperCroppie = ({file, isVerifying}) => {
         {!file.downloadURL && !isVerifying && <span>No File Uploaded</span>}
         {!file.downloadURL && isVerifying && <span>Is Verifying...</span>}
       </div>
+
+      <div className="row">
+        {mediaSource === 'file' && (
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span
+                className="input-group-text">
+                Upload Photo
+              </span>
+            </div>
+            <div className="custom-file">
+              <input
+                type="file"
+                className="custom-file-input"
+                onChange={handleChange}
+              />
+              <label
+                className="custom-file-label"
+              >
+                Choose file
+              </label>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
@@ -20,6 +48,7 @@ const FirebaseSuperCroppie = ({file, isVerifying}) => {
 FirebaseSuperCroppie.propTypes = {
   file: PropTypes.object.isRequired,
   isVerifying: PropTypes.bool.isRequired,
+  handleChange: PropTypes.func.isRequired,
 }
 
 export default connectFirebaseSuperCroppie({
--- studio/custom-components/FirebaseCroppieWithWebCam/V3/useFirebaseSuperCroppie.js
+++ studio/custom-components/FirebaseCroppieWithWebCam/V3/useFirebaseSuperCroppie.js
@@ -1,9 +1,18 @@
+import React from 'react'
 import useFileFromStorageRef from './useFileFromStorageRef'
+import useFileInput from './useFileInput'
+
+function dataURLDidChange({dataURL}) {
+  debugger
+}
 
 export default ({storageRef}) => {
   const {file, isVerifying} = useFileFromStorageRef({storageRef})
+  const {handleChange, dataURL} = useFileInput()
+  React.useEffect(dataURLDidChange.bind(null, {dataURL}), [dataURL])
   return {
     file,
     isVerifying,
+    handleChange,
   }
 }
