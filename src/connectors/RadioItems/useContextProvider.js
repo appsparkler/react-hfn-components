288304810ca517c5aeb7a3343303d7b4af6e7da8
Aakash Shah <ashah@lowes.com>
2020-03-17 11:12:03 +0530

    Moving connectors to src

---
+++ src/connectors/RadioItems/useContextProvider.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function handleChange({
+  setValue,
+}, evt) {
+  setValue(evt.target.value)
+}
+
+function resetValue({setValue}) {
+  setValue(null)
+}
+
+export default ({name, items}) => {
+  const [value, setValue] = React.useState(null)
+  return {
+    value,
+    name, items,
+    handleChange: handleChange.bind(null, {setValue}),
+    resetValue: resetValue.bind(null, {setValue}),
+  }
+}
