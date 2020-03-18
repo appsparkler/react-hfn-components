c38adcb7eb63710e5fa75418cc57656b740df3b8
Aakash Shah <ashah@lowes.com>
2020-03-18 18:48:59 +0530

    WIP

---
+++ studio/custom-components/FirebaseCroppieWithWebCam/V4/ProfilePhoto.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ProfilePhoto = ({file, isVerifying}) => (
+  <div>
+    {file && !isVerifying && (
+      <img className="img-thumbnail rounded-0" src={file.downloadURL} />
+    )}
+    {isVerifying && <span>is Verifying...</span>}
+  </div>
+)
+
+ProfilePhoto.propTypes = {
+  file: PropTypes.object,
+  isVerifying: PropTypes.bool.isRequired,
+}
+
+export default ProfilePhoto
--- studio/custom-components/FirebaseCroppieWithWebCam/V4/index.js
+++ studio/custom-components/FirebaseCroppieWithWebCam/V4/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import config from './config'
 import connectFirebaseSuperCroppie from './connectFirebaseSuperCroppie'
+import ProfilePhoto from './ProfilePhoto'
 
 const FirebaseSuperCroppie = ({
   file, isVerifying,
@@ -9,11 +10,8 @@ const FirebaseSuperCroppie = ({
   return (
     <div className="container mt-2">
       <div className="row">
-        <div className="col-2">
-          {file && !isVerifying && (
-            <img className="img-thumbnail" src={file.downloadURL} />
-          )}
-          {isVerifying && <span>is Verifying...</span>}
+        <div className="col-3">
+          <ProfilePhoto file={file} isVerifying={isVerifying} />
         </div>
       </div>
     </div>
