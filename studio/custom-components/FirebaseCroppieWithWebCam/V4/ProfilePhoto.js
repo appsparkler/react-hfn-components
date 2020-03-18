f818517532e7f999f501a83a17d3a2316701a32d
Aakash Shah <ashah@lowes.com>
2020-03-18 18:49:45 +0530

    formatting

--- studio/custom-components/FirebaseCroppieWithWebCam/V4/ProfilePhoto.js
+++ studio/custom-components/FirebaseCroppieWithWebCam/V4/ProfilePhoto.js
@@ -4,7 +4,10 @@ import PropTypes from 'prop-types'
 const ProfilePhoto = ({file, isVerifying}) => (
   <div>
     {file && !isVerifying && (
-      <img className="img-thumbnail rounded-0" src={file.downloadURL} />
+      <img
+        className="img-thumbnail rounded-0"
+        src={file.downloadURL}
+      />
     )}
     {isVerifying && <span>is Verifying...</span>}
   </div>
