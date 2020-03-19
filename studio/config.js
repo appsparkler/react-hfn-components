994f8e1e98d85b47db8fd8a873d1f60279294845
Aakash Shah <ashah@lowes.com>
2020-03-19 09:59:11 +0530

    clean up

--- studio/config.js
+++
@@ -1,8 +0,0 @@
-import FirebaseApp from '@react-hfn-studio/firebase-app'
-
-export const storageRef = FirebaseApp.storage().ref('INAAAEX32/profilePics1/')
-
-export default {
-  maxBytes: 2 * 1024 * 1024,
-  storageRef,
-}
