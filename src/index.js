42a30e634b6af7841aa9966216a840d7eb0904b1
Aakash Shah <ashah@lowes.com>
2020-03-21 15:02:31 +0530

    exporting required files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 export useFirebaseSuperCroppie  from '@react-hfn-hooks/useFirebaseSuperCroppie' //eslint-disable-line
+export useFirebaseFileInput from '@react-hfn-components/FirebaseFileInput/useFirebaseFileInput' //eslint-disable-line
