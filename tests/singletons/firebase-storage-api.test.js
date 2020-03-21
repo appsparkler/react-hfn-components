12cbcace26ee49d9eb6076e06e9c2d964e78caac
Aakash Shah <ashah@lowes.com>
2020-03-21 16:11:54 +0530

    clean up

--- tests/singletons/firebase-storage-api.test.js
+++
@@ -1,9 +0,0 @@
-import {FirebaseUtils} from '../../src'
-
-describe('Firebase Utils', () => {
-  it('should store the api passed to storage property', () => {
-    expect(FirebaseUtils.storage).toBe(undefined)
-    FirebaseUtils.storage = {}
-    expect(typeof FirebaseUtils.storage).toBe('object')
-  })
-})
