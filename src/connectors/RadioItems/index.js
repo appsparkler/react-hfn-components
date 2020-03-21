7de761b8175c4b71beca8ed249f68ff08c1ac7c6
Aakash Shah <ashah@lowes.com>
2020-03-21 15:26:01 +0530

    WIP

--- src/connectors/RadioItems/index.js
+++
@@ -1,6 +0,0 @@
-import RadioItemsContextProvider, {RadioItemsContext} from './context'
-import connector from './connector'
-
-export default connector({
-  Context: RadioItemsContext, Provider: RadioItemsContextProvider,
-})
