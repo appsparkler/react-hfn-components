ae694ca22bbff9e920a94a002d3c42efcca705ba
Aakash Shah <ashah@lowes.com>
2020-03-21 15:44:48 +0530

    WIP - copied files to src from studio

---
+++ src/components/FirebaseSuperCroppie/MediaSourceForm.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const MediaSourceForm = ({mediaSource, handleMediaSourceChange}) => (
+  <form>
+    <div className="form-check form-check-inline">
+      <input
+        name="mediaSource"
+        className="form-check-input"
+        type="radio"
+        value="file"
+        onChange={handleMediaSourceChange}
+        checked={mediaSource === 'file'}
+      />
+      <label className="form-check-label">
+        Upload From file
+      </label>
+    </div>
+    <div className="form-check form-check-inline">
+      <input
+        className="form-check-input"
+        type="radio"
+        name="mediaSource"
+        value="webcam"
+        onChange={handleMediaSourceChange}
+        checked={mediaSource === 'webcam'}
+      />
+      <label className="form-check-label" htmlFor="inlineRadio2">
+        Use Webcam
+      </label>
+    </div>
+  </form>
+)
+
+MediaSourceForm.propTypes = {
+  mediaSource: PropTypes.string,
+  handleMediaSourceChange: PropTypes.func.isRequired,
+}
+
+export default MediaSourceForm
