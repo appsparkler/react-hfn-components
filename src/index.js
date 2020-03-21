00a1afc2a6601d478ca3bbe951da50e6b0400126
Aakash Shah <ashah@lowes.com>
2020-03-21 16:10:40 +0530

    export file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export useFirebaseSuperCroppie  from '@react-hfn-components/FirebaseSuperCroppie/useFirebaseSuperCroppie' //eslint-disable-line
 export useFirebaseFileInput from '@react-hfn-components/FirebaseFileInput/useFirebaseFileInput' //eslint-disable-line
+export useFirebaseFileLink from '@react-hfn-components/FirebaseFileLink/useFirebaseFileLink'// eslint-disable-line
