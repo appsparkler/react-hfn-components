e27cfae4bb852a4e1d9b8f8150c754bd7cb4c78a
Aakash Shah <ashah@lowes.com>
2020-03-22 14:37:56 +0530

    setup

--- src/components/FirebaseFileInput/index.js
+++ src/components/FirebaseFileInput/index.js
@@ -1,45 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import FirebaseFileLink from './main'
 import useFirebaseFileInput from './useFirebaseFileInput'
 
 const FirebaseFileInput = ({storageRef}) => {
-  const {
-    handleFileInputChange,
-    isUploading,
-    error,
-    uploaded,
-    progress,
-    handleUploadButtonClick,
-    isVerifying,
-    firebaseFile,
-  } = useFirebaseFileInput({storageRef})
+  const config = useFirebaseFileInput({storageRef})
   return (
-    <div className="row">
-      <div className="col-12">
-        {isVerifying && '...isVerifying'}
-        {!isVerifying && firebaseFile?.downloadURL && (
-          <a href={firebaseFile.downloadURL}>
-            {firebaseFile.fileName || 'file'}
-          </a>
-        )}
-      </div>
-      <div className="col-12">
-        <input type="file" onChange={handleFileInputChange} />
-        <button
-          type="button"
-          className="btn btn-primary rounded-0"
-          disabled={isUploading}
-          onClick={handleUploadButtonClick}
-        >
-          Upload File
-        </button>
-        <br />
-        {isUploading && <progress value={progress} min="0" max="100" />}
-        <pre>{isUploading && '...is uploading'}</pre>
-        <pre>{uploaded && 'uploaded!'}</pre>
-        <pre>{error && error.message}</pre>
-      </div>
-    </div>
+    <FirebaseFileLink {...config} />
   )
 }
 
---
+++ src/components/FirebaseFileInput/main.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const FirebaseFileInput = ({
+  handleFileInputChange,
+  isUploading,
+  error,
+  uploaded,
+  progress,
+  handleUploadButtonClick,
+  isVerifying,
+  firebaseFile,
+}) => (
+  <div className="row">
+    <div className="col-12">
+      {isVerifying && '...isVerifying'}
+      {!isVerifying && firebaseFile?.downloadURL && (
+        <a href={firebaseFile.downloadURL}>
+          {firebaseFile.fileName || 'file'}
+        </a>
+      )}
+    </div>
+    <div className="col-12">
+      <input type="file" onChange={handleFileInputChange} />
+      <button
+        type="button"
+        className="btn btn-primary rounded-0"
+        disabled={isUploading}
+        onClick={handleUploadButtonClick}
+      >
+        Upload File
+      </button>
+      <br />
+      {isUploading && <progress value={progress} min="0" max="100" />}
+      <pre>{isUploading && '...is uploading'}</pre>
+      <pre>{uploaded && 'uploaded!'}</pre>
+      <pre>{error && error.message}</pre>
+    </div>
+  </div>
+)
+
+FirebaseFileInput.propTypes = {
+  handleFileInputChange: PropTypes.func.isRequired,
+  isUploading: PropTypes.bool.isRequired,
+  error: PropTypes.bool.isRequired,
+  uploaded: PropTypes.bool.isRequired,
+  progress: PropTypes.bool.isRequired,
+  handleUploadButtonClick: PropTypes.func.isRequired,
+  isVerifying: PropTypes.bool.isRequired,
+  firebaseFile: PropTypes.object,
+}
+
+export default FirebaseFileInput
