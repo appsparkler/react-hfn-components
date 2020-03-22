4dec6a83783a101c6e241618c9a063c4e52e99a2
Aakash Shah <ashah@lowes.com>
2020-03-22 14:18:24 +0530

    setup

--- studio/App.js
+++ studio/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import {Spinner, SpinnerSize} from 'office-ui-fabric-react'
 import './styles'
 
 const StudioApp = () => {
   return (
     <div className="ms-Grid" dir="ltr">
       <div className="ms-Grid-row">
-        <div className="ms-Grid-col ms-sm6 ms-md4 ms-lg2">A</div>
+        <div className="ms-Grid-col ms-sm6 ms-md4 ms-lg2">
+          <span><Spinner size={SpinnerSize.large} /></span>
+        </div>
         <div className="ms-Grid-col ms-sm6 ms-md8 ms-lg10">B</div>
       </div>
     </div>
