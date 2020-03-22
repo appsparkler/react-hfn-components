e0ac4cdcace6d8f8d17d215852e76f4b50986954
Aakash Shah <ashah@lowes.com>
2020-03-22 14:10:19 +0530

    set up office fabric

--- studio/App.js
+++ studio/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import FirebaseFileLink
-  from '@react-hfn-components/FirebaseFileLink'
-import config from './config'
 import './styles'
 
 const StudioApp = () => {
   return (
-    <div className="container mt-5">
-      <FirebaseFileLink {...config} />
-    </div>)
+    <div className="ms-Grid" dir="ltr">
+      <div className="ms-Grid-row">
+        <div className="ms-Grid-col ms-sm6 ms-md4 ms-lg2">A</div>
+        <div className="ms-Grid-col ms-sm6 ms-md8 ms-lg10">B</div>
+      </div>
+    </div>
+  )
 }
 
 export default StudioApp
--- studio/index.js
+++ studio/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import {initializeIcons} from '@uifabric/icons'
+import 'office-ui-fabric-react/dist/css/fabric.min.css'
+
+initializeIcons()
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
