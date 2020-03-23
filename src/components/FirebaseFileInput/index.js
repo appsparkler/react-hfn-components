1c6ef95c9525499bcc1618ad5bbeeff5acd633e0
Aakash Shah <ashah@lowes.com>
2020-03-23 08:02:40 +0530

    Removed un-utilized files

--- src/components/FirebaseFileInput/index.js
+++ src/components/FirebaseFileInput/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import FirebaseFileLink from './main'
+import FirebaseFileInput from './main'
 import useFirebaseFileInput from './useFirebaseFileInput'
 
-const FirebaseFileInput = ({storageRef}) => {
+const UsedFirebaseFileInput = ({storageRef}) => {
   const config = useFirebaseFileInput({storageRef})
   return (
-    <FirebaseFileLink {...config} />
+    <FirebaseFileInput {...config} />
   )
 }
 
-FirebaseFileInput.propTypes = {
+UsedFirebaseFileInput.propTypes = {
   storageRef: PropTypes.object.isRequired,
 }
 
-export default FirebaseFileInput
+export default UsedFirebaseFileInput
