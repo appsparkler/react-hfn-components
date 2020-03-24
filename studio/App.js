31e9d2bfee1dc83e0279196bb16dcd621be4ad4b
Aakash Shah <ashah@lowes.com>
2020-03-24 07:25:30 +0530

    WIP

--- studio/App.js
+++ studio/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import FirebaseFileInput from '@react-hfn-components/FirebaseFileInput'
+import FirebaseSuperCroppie from '@react-hfn-components/FirebaseSuperCroppie'
 import config from './config'
 import './styles'
 
 const StudioApp = () => {
   return (
     <div className="ms-Grid" dir="ltr">
-      <FirebaseFileInput {...config} />
+      <FirebaseSuperCroppie {...config} />
     </div>
   )
 }
