87f77b6163d1f1db82d2a5c3d06de271123ec922
Aakash Shah <ashah@lowes.com>
2020-03-25 16:31:31 +0530

    Added all three components to studio

--- studio/App.js
+++ studio/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import FirebaseFileLink from '@react-hfn-components/FirebaseFileLink'
+import FirebaseFileInput from '@react-hfn-components/FirebaseFileInput'
+import FirebaseSuperCroppie from '@react-hfn-components/FirebaseSuperCroppie'
 import config from './config'
 import './styles'
 
 const StudioApp = () => {
   return (
     <div className="ms-Grid" dir="ltr">
+      <h3>Firebase File Link</h3>
       <FirebaseFileLink {...config} />
+      <h3>Firebase File Input</h3>
+      <FirebaseFileInput {...config} />
+      <h3>Firebase Super Croppie</h3>
+      <FirebaseSuperCroppie {...config} />
     </div>
   )
 }
