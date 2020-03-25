37f87bbae64c65a8f3521764c8e3f8a0411b0a70
Aakash Shah <ashah@lowes.com>
2020-03-25 12:54:30 +0530

    WIP

--- studio/App.js
+++ studio/App.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import FirebaseSuperCroppie from '@react-hfn-components/FirebaseSuperCroppie'
+import FirebaseFileLink from '@react-hfn-components/FirebaseFileLink'
 import config from './config'
-// import {Stack, DefaultButton} from 'office-ui-fabric-react'
-// import PreviewModal from './PreviewModal'
 import './styles'
 
 const StudioApp = () => {
   return (
     <div className="ms-Grid" dir="ltr">
-      {/*  <Stack horizontal tokens={{childrenGap: '20'}}>
-        <DefaultButton primary text="Upload" />
-        <PreviewModal imgSrc="http://placekitten.com/120/80" />
-      </Stack>*/}
-      <FirebaseSuperCroppie {...config} />
+      <FirebaseFileLink {...config} />
     </div>
   )
 }
