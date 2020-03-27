2656f5075e45e8e4f69b4d3fbd18c2c9a9af96c0
Aakash Shah <ashah@lowes.com>
2020-03-27 06:21:26 +0530

    WIP

--- src/hoc/FileInputButton.js
+++ src/hoc/FileInputButton.js
@@ -2,28 +2,38 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {DefaultButton} from 'office-ui-fabric-react'
 
+// const inputWrapperStyles = {
+//   position: 'absolute',
+//   left: 0, right: 0,
+//   bottom: 0, top: 0,
+//   opacity: 0,
+// }
+
 const inputStyles = {
   position: 'absolute',
   left: 0, right: 0,
   bottom: 0, top: 0,
   opacity: 0,
+  width: '100%',
 }
 
 const FileInputButton = ({
   primary, secondary, onChange, disabled, text,
 }) => (
-  <div style={{position: 'relative'}}>
+  <div >
     <DefaultButton
+      style={{root: {position: 'relative'}}}
       primary={secondary}
       secondary={primary}
       disabled={disabled}
       text={text}
-    />
-    <input type='file'
-      style={inputStyles}
-      disabled={disabled}
-      onChange={onChange}
-    />
+    >
+      <input type='file'
+        style={inputStyles}
+        disabled={disabled}
+        onChange={onChange}
+      />
+    </DefaultButton>
   </div>
 )
 
