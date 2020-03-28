2aecc9176ffe6500d001270d80d587245d53b517
Aakash Shah <ashah@lowes.com>
2020-03-28 15:22:48 +0530

    WIP

--- studio/App.js
+++ studio/App.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import {MemoMediaSourceForm} from '@react-hfn-hoc/MediaSourceForm'
 import './styles'
 
 const StudioApp = () => {
   return (
     <div className="ms-Grid" dir="ltr">
-      <MemoMediaSourceForm
-        mediaSource={'webcam'}
-        disabled
-      />
+
     </div>
   )
 }
---
+++ studio/components/MediaSourceForm.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {ChoiceGroup} from 'office-ui-fabric-react'
+import './styles'
+
+const options = [
+  {key: 'file', text: 'Upload', iconProps: {iconName: 'CloudUpload'}},
+  {key: 'webcam', text: 'Webcam', iconProps: {iconName: 'Webcam2'}},
+]
+
+const StudioApp = () => {
+  const [mediaSource, setMediaSource] = React.useState(null)
+  return (
+    <div className="ms-Grid" dir="ltr">
+      <ChoiceGroup
+        label="Upload photo"
+        selectedKey={mediaSource}
+        options={options}
+        onChange={(evt, selectedOption) => {
+          const value = selectedOption.key
+          setMediaSource(value)
+        }}
+      />
+      {mediaSource && mediaSource}
+    </div>
+  )
+}
+
+export default StudioApp
